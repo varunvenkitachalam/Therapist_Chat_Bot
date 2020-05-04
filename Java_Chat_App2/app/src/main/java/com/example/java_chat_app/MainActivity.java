7a3eecf616baa31a598f461d2f7c2cab21a11a9a
Varun Venkitachalam <Varun.Venkitachalam@gmail.com>
2020-05-04 16:53:07 -0700

    integrating Version1 of heroku server for bot response

--- Java_Chat_App2/app/src/main/java/com/example/java_chat_app/MainActivity.java
+++ Java_Chat_App2/app/src/main/java/com/example/java_chat_app/MainActivity.java
@@ -6,12 +6,28 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
@@ -30,21 +46,15 @@ public class MainActivity extends AppCompatActivity {
         messageAdapter = new MessageAdapter(messageResponseList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
         recyclerView.setAdapter(messageAdapter);
+        MessageResponse firstMessage = new MessageResponse("Thanks for waiting! I am Eve, your personal Therapy Chatbot. Feel free to talk to me or ask me any questions!", false);
+        messageResponseList.add(firstMessage);
         userInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if(actionId == EditorInfo.IME_ACTION_SEND){
                     MessageResponse message = new MessageResponse(userInput.getText().toString(), true);
                     messageResponseList.add(message);
-                    MessageResponse message2 = new MessageResponse(userInput.getText().toString(), false);
-                    messageResponseList.add(message2);
-                    if(!isVisible()){
-                        recyclerView.smoothScrollToPosition(messageAdapter.getItemCount()-1);
-                    }
-                    EditText editText = (EditText) findViewById(R.id.userInput);
-                    editText.getText().clear();
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(userInput.getWindowToken(), 0);
+                    getResponse(userInput.getText().toString());
                 }
                 return true;
             }
@@ -56,5 +66,73 @@ public class MainActivity extends AppCompatActivity {
         int positionOfLastVisibleItem = linearLayoutManager.findLastCompletelyVisibleItemPosition();
         int itemCount = recyclerView.getAdapter().getItemCount();
         return (positionOfLastVisibleItem>=itemCount);
+    }
+    public void getResponse(String input){
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "https://psychbot-cs4800.herokuapp.com/api/send_message";
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("message", input);
+             //String serverResponse;
+            final String requestBody = jsonBody.toString();
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject respObject = new JSONObject(response);
+                        String serverResponse = respObject.getString("message");
+                        Log.i("VOLLEY", serverResponse);
+                        MessageResponse message2 = new MessageResponse(serverResponse, false);
+                        messageResponseList.add(message2);
+                        if(!isVisible()){
+                            recyclerView.smoothScrollToPosition(messageAdapter.getItemCount()-1);
+                        }
+                        EditText editText = (EditText) findViewById(R.id.userInput);
+                        editText.getText().clear();
+                        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(userInput.getWindowToken(), 0);
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                    }
+                    return super.parseNetworkResponse(response);
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
